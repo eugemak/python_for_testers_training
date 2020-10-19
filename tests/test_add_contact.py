3850c4753922dae9deac1677a5c7b60292907f5f
euge_mak <BW#}R1YPn>
2020-10-19 15:48:52 +0300

    task 15. random parametrize

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -1,13 +1,41 @@
 # -*- coding: utf-8 -*-
 from model.contact_m import Contact
+import random
+import string
+import pytest
 
 
-def test_add_user(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [
+    Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 10),
+            lastname=random_string("lastname", 10), title=random_string("title", 10),
+            company=random_string("company", 10), home_phone=random_string("company", 10))
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_user(app, contact):
     old_users = app.contact.get_contacts_list(second_iteration=False)
-    new_user = Contact(firstname="Carlos", middlename="Карлович", lastname="Sainz",
-                       title="Driver", company="McLaren F1 Team", home_phone="000-000-00")
-    app.contact.create_new_user(new_user)
+    app.contact.create_new_user(contact)
     assert len(old_users) + 1 == app.contact.count()
     new_users = app.contact.get_contacts_list(second_iteration=False)
-    old_users.append(new_user)
+    old_users.append(contact)
     assert sorted(old_users, key=Contact.user_id_or_max) == sorted(new_users, key=Contact.user_id_or_max)
+
+
+# def test_add_user(app):
+#     old_users = app.contact.get_contacts_list(second_iteration=False)
+#     new_user = Contact(firstname="Carlos", middlename="Карлович", lastname="Sainz",
+#                        title="Driver", company="McLaren F1 Team", home_phone="000-000-00")
+#     app.contact.create_new_user(new_user)
+#     assert len(old_users) + 1 == app.contact.count()
+#     new_users = app.contact.get_contacts_list(second_iteration=False)
+#     old_users.append(new_user)
+#     assert sorted(old_users, key=Contact.user_id_or_max) == sorted(new_users, key=Contact.user_id_or_max)
+
+
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,11 +1,33 @@
 # -*- coding: utf-8 -*-
-
+import pytest
 from model.group_m import Group
+import random
+import string
 
 
-def test_add_group(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
+    for i in range(5)
+]
+
+# testdata = [
+#     Group(name=name, header=header, footer=header)
+#     for name in ["", random_string("name", 10)]
+#     for header in ["", random_string("header", 20)]
+#     for footer in ["", random_string("footer", 20)]
+# ]
+
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
+    pass
     old_groups = app.group.get_group_list()
-    group = Group(name="First", header="Задание №1", footer="Футер задания №1")
+    # group = Group(name="First", header="Задание №1", footer="Футер задания №1")
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
