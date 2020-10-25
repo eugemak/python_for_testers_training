d8b233663289ebfdd028a1896d5e4cc2d5663fef
euge_mak <BW#}R1YPn>
2020-10-25 09:54:48 +0300

    task 19. use test data from json

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -11,3 +11,13 @@ def test_add_user(app, data_contacts):
     new_users = app.contact.get_contacts_list()
     old_users.append(contact)
     assert sorted(old_users, key=Contact.user_id_or_max) == sorted(new_users, key=Contact.user_id_or_max)
+
+
+def test_add_user2(app, json_contacts):
+    contact = json_contacts
+    old_users = app.contact.get_contacts_list()
+    app.contact.create_new_user(contact)
+    assert len(old_users) + 1 == app.contact.count()
+    new_users = app.contact.get_contacts_list()
+    old_users.append(contact)
+    assert sorted(old_users, key=Contact.user_id_or_max) == sorted(new_users, key=Contact.user_id_or_max)
