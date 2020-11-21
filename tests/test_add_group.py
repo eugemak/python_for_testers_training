9e2b7d2ae5b27ed522f7d943491d19c44a7c54bf
euge_mak <BW#}R1YPn>
2020-11-21 11:48:11 +0300

    pytest-allure

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from model.group_m import Group
+import allure
 
 
 # # Загрузка при использовании данных из отдельного модуля
@@ -12,18 +13,21 @@ from model.group_m import Group
 #     new_groups = app.group.get_group_list()
 #     old_groups.append(group)
 #     assert sorted(old_groups, key=Group.group_id_or_max) == sorted(new_groups, key=Group.group_id_or_max)
-#
-#
-# # Загрузка при использовании данных из сгенерированного json-файла
-# def test_add_group_from_json(app, json_groups):
-#     group = json_groups
-#     old_groups = app.group.get_group_list()
-#     app.group.create(group)
-#     assert len(old_groups) + 1 == app.group.count()
-#     new_groups = app.group.get_group_list()
-#     old_groups.append(group)
-#     assert sorted(old_groups, key=Group.group_id_or_max) == sorted(new_groups, key=Group.group_id_or_max)
-#
+
+
+# Загрузка при использовании данных из сгенерированного json-файла
+def test_add_group_from_json(app, json_groups):
+    group = json_groups
+    with allure.step('Given a group list'):
+        old_groups = app.group.get_group_list()
+    with allure.step('When I add a group %s to the list' % group):
+        app.group.create(group)
+    assert len(old_groups) + 1 == app.group.count()
+    with allure.step('Then the new group list is equal to the old list with the added group'):
+        new_groups = app.group.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.group_id_or_max) == sorted(new_groups, key=Group.group_id_or_max)
+
 #
 # # Сравнение с данными из БД
 # def test_add_group_db_assert(app, db, json_groups):
@@ -33,15 +37,15 @@ from model.group_m import Group
 #     new_groups = db.get_group_list()
 #     old_groups.append(group)
 #     assert sorted(old_groups, key=Group.group_id_or_max) == sorted(new_groups, key=Group.group_id_or_max)
-
-
-# Сравнение с данными из БД через ORM
-def test_add_group_db_orm_assert(app, orm, check_ui, json_groups):
-    group = json_groups
-    old_groups = orm.get_group_list()
-    app.group.create(group)
-    new_groups = orm.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.group_id_or_max) == sorted(new_groups, key=Group.group_id_or_max)
-    if check_ui:
-        assert sorted(new_groups, key=Group.group_id_or_max) == sorted(app.group.get_group_list(), key=Group.group_id_or_max)
+#
+#
+# # Сравнение с данными из БД через ORM
+# def test_add_group_db_orm_assert(app, orm, check_ui, json_groups):
+#     group = json_groups
+#     old_groups = orm.get_group_list()
+#     app.group.create(group)
+#     new_groups = orm.get_group_list()
+#     old_groups.append(group)
+#     assert sorted(old_groups, key=Group.group_id_or_max) == sorted(new_groups, key=Group.group_id_or_max)
+#     if check_ui:
+#         assert sorted(new_groups, key=Group.group_id_or_max) == sorted(app.group.get_group_list(), key=Group.group_id_or_max)
